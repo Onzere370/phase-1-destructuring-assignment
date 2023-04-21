d5aa43ca5052fbc261ec219a7b4c416ef9bd2a7f
cynthia <cynthiakagz@gmail.com>
2023-04-22 00:14:57 +0300

    completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = ('cow, horse, sheep, pig, chicken');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
@@ -9,6 +9,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+console.log(muppet);
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -21,9 +22,55 @@ const nestedMuppet = {
       song4: 'I Hope That Something Better Comes Along',
     },
   },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
 };
+console.log(nestedMuppet);
+ const animalsSound = [ "moo","neigh","baa", "oink", "cluck"];
+ const moo = 'cow'
+ const neigh = 'horse'
+ const baa = 'sheep'
+ const oink = 'pig'
+ const cluck = 'chicken'
+
+const bessie = 'cow'
+const dolly = 'sheep'
+const babe = 'pig'
+const little = 'chicken'
+
+const blackAndWhite = 'cow'
+const black = 'sheep'
+const pink = 'pig'
+const sevenColors = ["red","orange","yellow","green","blue","indigo","violet"]
+const red = 'red'
+const orange = 'orange'
+const yellow = 'yellow'
+const green = 'green'
+const blue = 'blue'
+const indigo = 'indigo'
+const violet = 'violet'
+
+const sixColors = ["yellow","orange","red","blue","green","violet"]
+const y = 'yellow'
+const o = 'orange'
+const r = 'red'
+const b = 'blue'
+const g = 'green'
+const v = 'violet'
+console.log(sixColors);
+
+const oneColor = ["indigo"]
+const indg = 'indigo'
+  
+ const muppetName = 'Miss Piggy'
+ const color = 'pink'
+ const song = 'Never Before, Never Again'
+ const job = 'Cast member of The Muppet Show'
+  const partner = 'Kermit'
+
+ const nestedJob = 'Host of The Muppet Show'
+ const song2 = 'Moving Right Along'
+ const song4 = 'I Hope That Something Better Comes Along'
+ const nestedPartner = 'Miss Piggy'
+
 
 // Strings
 
